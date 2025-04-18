1d456cbf94b9ea76663e38b0ff830ac2d4eea769
DaviRic <davi.r.cruvel@gmail.com>
2025-04-18 01:33:29 -0400

    [feat] Ingestão dos dados de train.csv e store.csv para o Spark

---
+++ scripts/etl/ingest_raw_data.py
@@ -0,0 +1,33 @@
+from pyspark.sql import SparkSession # type: ignore
+
+spark = SparkSession.builder.appName("Rossmann Store Sales").getOrCreate()
+
+# Caminho dos arquivos
+train_path = "data/raw/rossmann_store_sales/train.csv"
+store_parh = "data/raw/rossmann_store_sales/store.csv"
+
+# Lê o arquivo de vendas (treinamento)
+train_df = spark.read.csv(train_path, header=True, inferSchema=True)
+
+# Lê os arquivos com os dados das lojas
+store_df = spark.read.csv(store_parh, header=True, inferSchema=True)
+
+# Mostrar o esquema para garantir que tudo foi lido corretamente
+print("Schema - Train:")
+train_df.printSchema()
+
+print("Schema - Store")
+store_df.printSchema()
+
+# Mostra as primeiras linhas
+print("Train sample")
+train_df.show(5)
+
+print("Store sample")
+store_df.show(5)
+
+# Faz o join entre dois DataFrames pela coluna "store" para uma visão combinada
+joined_df = train_df(store_df, on="Store", how="left")
+
+print("Joined sample")
+joined_df.show(5)
